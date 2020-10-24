caa09a34964ce50f19c4e1aad06bbd41296ef477
Gabriel Cánepa <gacanepa@gmail.com>
2020-10-24 14:19:55 -0300

    Added style using a variable

--- college-app/src/components/Instructor.js
+++ college-app/src/components/Instructor.js
@@ -2,7 +2,8 @@ import React from 'react';
 
 // Create functional component and export in one step
 export default function Instructor({name, experienceYears}) {
+    const experienceStyles = {color: experienceYears > 10 ? "green" : "red"}
     return (
-        <p style={{color: experienceYears > 10 ? "green" : "red"}}>{name} has {experienceYears} years of experience.</p>
+        <p style={experienceStyles}>{name} has {experienceYears} years of experience.</p>
     )
 }
